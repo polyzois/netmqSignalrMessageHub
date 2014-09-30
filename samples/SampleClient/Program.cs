04566d631c35fb999b4a8f2ef35963d9f6b40af7
Konstantin Polyzois <konstantin.polyzois@nasdaqomx.com>
2014-09-30 11:13:30 +0200

    Added sample client

---
+++ samples/SampleClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+using Microsoft.AspNet.SignalR.Client.Hubs;
+
+namespace Microsoft.AspNet.SignalR.Client40.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string serverUrl = "http://localhost:8080/";
+
+            if (args.Length > 0) {
+                serverUrl = args [0];
+            }
+
+            var writer = Console.Out;
+            var client = new Client(writer);
+
+           
+
+            client.Run(serverUrl);
+
+            Console.ReadKey();
+        }
+    }
+}
